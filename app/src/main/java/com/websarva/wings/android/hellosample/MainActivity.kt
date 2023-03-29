cafa293b656f6e260388ae8125087340857ecf47
eiki-ogawa <eiki-ogawa@x-power.co.jp>
2023-03-29 10:22:18 +0900

    init

---
+++ app/src/main/java/com/websarva/wings/android/hellosample/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.websarva.wings.android.hellosample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btClick = findViewById<Button>(R.id.btClick)
+        val btClear = findViewById<Button>(R.id.btClear)
+        val listener = HelloListener()
+        btClick.setOnClickListener(listener)
+        btClear.setOnClickListener(listener)
+
+
+    }
+
+    private inner class HelloListener : View.OnClickListener {
+        override fun onClick(view: View) {
+            val input = findViewById<EditText>(R.id.etName)
+            val output = findViewById<TextView>(R.id.tvOutput)
+
+            when(view.id) {
+                R.id.btClick -> {
+                    val inputStr = input.text.toString()
+
+                    output.text = inputStr + "さん、こんにちは！"
+                }
+
+                R.id.btClear -> {
+                    input.setText("")
+
+                    output.text = ""
+                }
+            }
+        }
+    }
+}
